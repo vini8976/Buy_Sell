767ececb44c1235b4fba464635364867cb756e21
Kudumula Surya <suryakudumuala29@gmail.com>
2025-07-21 12:54:58 +0530

    updaed vercel

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  base: '/',
-});
+})
